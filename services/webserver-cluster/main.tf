60d5e8befea623ac7025f884933d18e8354d5a59
MT <mtrojas@protonmail.com>
2021-09-10 08:46:05 -0300

    feat: generate tags dynamically

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -33,6 +33,16 @@ resource "aws_autoscaling_group" "asg-servers" {
     value               = "${var.cluster_name}-asg-servers"
     propagate_at_launch = true
   }
+
+  dynamic "tag" {
+    for_each = var.custom_tags
+
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
 }
 
 resource "aws_lb" "alb-servers" {
