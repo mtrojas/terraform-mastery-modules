c8fcaad6f794b3f02953aa60bbed5e5093e0a779
MT <mtrojas@protonmail.com>
2021-09-28 08:17:27 -0300

    feat: add cloudwatch alarms

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -166,6 +166,42 @@ resource "aws_security_group_rule" "allow_tcp_inbound" {
   cidr_blocks = local.all_ips
 }
 
+resource "aws_cloudwatch_metric_alarm" "high_cpu_utilization" {
+  alarm_name  = "${var.cluster_name}-high-cpu-utilization"
+  namespace   = "AWS/EC2"
+  metric_name = "CPUUtilization"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.asg-servers.name
+  }
+
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  period              = 300
+  statistic           = "Average"
+  threshold           = 90
+  unit                = "Percent"
+}
+
+resource "aws_cloudwatch_metric_alarm" "low_cpu_credit_balance" {
+  count = format("%.1s", var.instance_type) == "t" ? 1 : 0
+
+  alarm_name  = "${var.cluster_name}-low-cpu-credit-balance"
+  namespace   = "AWS/EC2"
+  metric_name = "CPUCreditBalance"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.asg-servers.name
+  }
+
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = 1
+  period              = 300
+  statistic           = "Minimum"
+  threshold           = 10
+  unit                = "Count"
+}
+
 data "template_file" "user_data" {
   template = file("${path.module}/user-data.sh")
 
