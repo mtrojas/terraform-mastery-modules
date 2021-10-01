7edbd664e1b44a481a3db65259647f0c114f48ca
MT <mtrojas@protonmail.com>
2021-10-01 23:57:42 +0100

    feat: use if-else to conditionally use a different user_data script

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -11,7 +11,12 @@ resource "aws_launch_configuration" "servers" {
   image_id        = var.ami
   instance_type   = var.instance_type
   security_groups = [aws_security_group.sg.id]
-  user_data       = data.template_file.user_data.rendered
+
+  user_data = (
+    length(data.template_file.user_data[*]) > 0
+    ? data.template_file.user_data[0].rendered
+    : data.template_file.user_data_new[0].rendered
+  )
 
   lifecycle {
     create_before_destroy = true
@@ -203,12 +208,24 @@ resource "aws_cloudwatch_metric_alarm" "low_cpu_credit_balance" {
 }
 
 data "template_file" "user_data" {
+  count = var.enable_new_user_data ? 0 : 1
+
   template = file("${path.module}/user-data.sh")
 
   vars = {
     server_port = var.server_port
     db_address  = data.terraform_remote_state.db.outputs.address
     db_port     = data.terraform_remote_state.db.outputs.port
+  }
+}
+
+data "template_file" "user_data_new" {
+  count = var.enable_new_user_data ? 1 : 0
+
+  template = file("${path.module}/user-data-new.sh")
+
+  vars = {
+    server_port = var.server_port
   }
 }
 
