c0ec5a07fb8d19f509443aa8f36541282a086bea
MT <mtrojas@protonmail.com>
2021-10-11 20:53:39 +0100

    feat: add examples for asg module

---
+++ examples/asg/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  backend "s3" {
+    key = "modules/examples/asg/terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "asg" {
+  source = "../../cluster/asg-rolling-deploy"
+
+  cluster_name  = var.cluster_name
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  min_size           = 1
+  max_size           = 1
+  enable_autoscaling = false
+
+  subnet_ids = data.aws_subnet_ids.default.ids
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
---
+++ examples/asg/variables.tf
@@ -0,0 +1,5 @@
+variable "cluster_name" {
+  description = "The name of the ASG and all its resources"
+  type        = string
+  default     = "example-asg-terraform-mastery"
+}
