63477cb95227dc1b8fb2bd84187859cf4ddcf662
MT <mtrojas@protonmail.com>
2021-10-16 14:14:09 +0100

    chore: cleanup code and add comment for required parameters

--- data-stores/mysql/variables.tf
+++ data-stores/mysql/variables.tf
@@ -1,9 +1,20 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "db_name" {
+  description = "The name to use for the database"
+  type        = string
+}
+
+variable "db_username" {
+  description = "The username for the database"
+  type        = string
+}
+
 variable "db_password" {
   description = "The password for the database"
   type        = string
 }
 
-variable "cluster_name" {
-  description = "The name to use for all the cluster resources"
-  type        = string
-}
