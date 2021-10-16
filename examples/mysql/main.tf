e8b2da9074599ba4f6b28e58bcda5fa4edd93b0d
MT <mtrojas@protonmail.com>
2021-10-16 16:25:59 +0100

    fix: move provider version constraint into a required_providers block

--- examples/mysql/main.tf
+++ examples/mysql/main.tf
@@ -2,6 +2,13 @@ terraform {
   # Require Terraform at exactly version 1.0.9
   required_version = "= 1.0.9"
 
+  required_providers {
+    # Allow any 3.x version of the AWS provider
+    aws = {
+      version = "~> 3.0"
+    }
+  }
+
   backend "s3" {
     key = "modules/examples/mysql/terraform.tfstate"
   }
@@ -9,9 +16,6 @@ terraform {
 
 provider "aws" {
   region = "us-east-2"
-
-  # Allow any 3.x version of the AWS provider
-  version = "~> 3.0"
 }
 
 module "mysql" {
