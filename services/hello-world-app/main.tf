d3e40a610b7e32d0d24827506b63a405a1d4658f
MT <mtrojas@protonmail.com>
2021-11-30 20:34:11 +0000

    refactor: separate dependencies and add as input variables

--- services/hello-world-app/main.tf
+++ services/hello-world-app/main.tf
@@ -73,24 +73,6 @@ resource "aws_lb_listener_rule" "alb_listener_rule" {
   }
 }
 
-data "terraform_remote_state" "db" {
-  backend = "s3"
-
-  config = {
-    bucket = var.db_remote_state_bucket
-    key    = var.db_remote_state_key
-    region = "us-east-2"
-  }
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
-}
-
 locals {
   http_protocol = "HTTP"
 }
